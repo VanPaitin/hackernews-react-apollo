d150de4f4c1070a7b18b80b7ad325927000d304a
Mayowa Pitan <mayowasolomon@gmail.com>
2023-05-09 03:13:53 +0100

    add ability to create link

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import CreateLink from './CreateLink';
+import LinkList from './LinkList';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <LinkList />
+        <CreateLink />;
+      </>
+    )
+  }
+}
+
+export default App;
